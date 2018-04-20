002aa273bb9e6f91f4a264c9d383e2ec43a0b88f
Ilyin Andrey <ilin-andrew@outlook.com>
2018-04-20 16:03:00 -0700

    wpf boxes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+
+                var PSScript = "param($param1) $d = get - date; $s = 'test string value'; " +
+                "$d; $s; $param1; get-service";
+
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                PowerShellInstance.AddScript(PSScript);
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                //PowerShellInstance.AddParameter("param1", "parameter 1 value!");
+
+                // invoke execution on the pipeline (collecting output)
+                Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+
+                // check the other output streams (for example, the error stream)
+                if (PowerShellInstance.Streams.Error.Count > 0)
+                {
+                    // error records were written to the error stream.
+                    // do something with the items found.
+                    Console.WriteLine(PowerShellInstance.Streams.Error.ToString());
+                }
+
+                // loop through each output object item
+                foreach (PSObject outputItem in PSOutput)
+                {
+                    // if null object was dumped to the pipeline during the script then a null
+                    // object may be present here. check for null to prevent potential NRE.
+
+                    if (outputItem == null)
+
+                    {
+                        Console.WriteLine("Null PS object was returned during the script");
+                    }
+
+                    if (outputItem != null)
+
+                    {
+                        //TODO: do something with the output item  
+                        Console.WriteLine(outputItem.BaseObject.GetType().FullName);
+                        Console.WriteLine(outputItem.BaseObject.ToString() + "\n");
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+
+                var PSScript = "get-service";
+
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                PowerShellInstance.AddScript(PSScript);
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                //PowerShellInstance.AddParameter("param1", "parameter 1 value!");
+
+                // invoke execution on the pipeline (collecting output)
+                Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+
+                // check the other output streams (for example, the error stream)
+                if (PowerShellInstance.Streams.Error.Count > 0)
+                {
+                    // error records were written to the error stream.
+                    // do something with the items found.
+                    TextBox1.Text += PowerShellInstance.Streams.Error.ToString();
+                }
+
+                // loop through each output object item
+                foreach (PSObject outputItem in PSOutput)
+                {
+                    // if null object was dumped to the pipeline during the script then a null
+                    // object may be present here. check for null to prevent potential NRE.
+                    
+                    if (outputItem == null)
+
+                    {
+                        TextBox1.Text += "Null PS object was returned during the script";
+                    }
+
+                    if (outputItem != null)
+
+                    {
+                        //TODO: do something with the output item  
+                        TextBox1.AppendText(outputItem.BaseObject.ToString());
+                        TextBox1.Text += "1";//outputItem.BaseObject.ToString();
+                    }
+                }
+
+
+
+
+            }
+
+
+        }
+
+ 
+    }
+}
